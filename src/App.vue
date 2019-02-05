4c6402ff41eb063894bb1ab34b945ef0d22f37cb
shesw <anjicsl14@gmail.com>
2019-02-05 16:19:37 +0800

    change title and icon

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,21 @@
 </template>
 
 <script>
-export default {
-  name: "App",
-  data() {
-    return {};
-  }
-};
+  export default {
+    name: "App",
+    data() {
+      return {};
+    }
+  };
 </script>
 
 <style scoped>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
